0df657446cac0790245dd06c8c9dc602b765d373
YONG WEN LI <TP045014@mail.apu.edu.my>
2020-02-16 19:30:53 +0800

    Add migration and hotel controller

---
+++ ddacAPI/Data/ddacAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ddacAPI.Models;
+
+namespace ddacAPI.Data
+{
+    public class ddacAPIContext : DbContext
+    {
+        public ddacAPIContext (DbContextOptions<ddacAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ddacAPI.Models.Hotel> Hotel { get; set; }
+    }
+}
