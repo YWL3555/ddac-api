6e65550d260207268176b765931313e918e8b0c9
YONG WEN LI <TP045014@mail.apu.edu.my>
2020-02-17 22:23:13 +0800

    Add models

---
+++ ddacAPI/Models/Facility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ddacAPI.Models
+{
+    public class Facility
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
