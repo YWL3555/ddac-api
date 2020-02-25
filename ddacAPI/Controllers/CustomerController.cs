1d8a5a89787f284554328cf19036b442872b64ef
YONG WEN LI <TP045014@mail.apu.edu.my>
2020-02-25 16:01:30 +0800

    Update customer api

--- ddacAPI/Controllers/CustomerController.cs
+++ ddacAPI/Controllers/CustomerController.cs
@@ -101,6 +101,41 @@ namespace ddacAPI.Controllers
                 return BadRequest(new { message = "Email or password is incorrect." });
         }
 
+        // POST: api/customer/book
+        [HttpPost("book")]
+        [Authorize(Roles = "Customer")]
+        public async Task<IActionResult> PostBooking([FromBody] Booking booking)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            string customerId = User.Claims.First(c => c.Type == "UserID").Value;
+
+            var room = await _context.RoomType.FindAsync(booking.RoomTypeId);
+
+            var days = (booking.EndDate - booking.StartDate).TotalDays;
+
+            booking.TotalAmount = room.Price * Convert.ToDecimal(days);
+
+            var bookingToBeCreated = new Booking()
+            {
+                CustomerId = customerId,
+                RoomTypeId = booking.RoomTypeId,
+                TotalAmount = booking.TotalAmount,
+                StartDate = booking.StartDate,
+                EndDate = booking.EndDate,
+                Reviewed = false,
+                Canceled = false
+            };
+
+            _context.Booking.Add(bookingToBeCreated);
+            await _context.SaveChangesAsync();
+
+            return Ok(bookingToBeCreated);
+        }
+
         [HttpGet]
         [Authorize(Roles ="Customer")]
         [Route("profile")]
@@ -124,7 +159,7 @@ namespace ddacAPI.Controllers
         [HttpPut]
         [Authorize(Roles = "Customer")]
         [Route("profile")]
-        //GET: /api/customer/profile
+        //PUT: /api/customer/profile
         public async Task<IActionResult> EditUserProfile([FromBody] Customer customer)
         {
             
@@ -146,8 +181,8 @@ namespace ddacAPI.Controllers
 
         [HttpPost]
         [Authorize(Roles = "Customer")]
-        [Route("upload")]
-        //GET: /api/customer/upload
+        [Route("pic-upload")]
+        //POST: /api/customer/pic-upload
         public async Task<IActionResult> UploadUserProfilePic(IFormFile newFile)
         {
             _blobManager = new BlobManager(Configuration);
@@ -180,5 +215,155 @@ namespace ddacAPI.Controllers
             return NoContent();
 
         }
+
+        // GET: api/customer/bookings
+        [HttpGet("bookings")]
+        public object GetBookingsByCustomer()
+        {
+            string customerId = User.Claims.First(c => c.Type == "UserID").Value;
+
+            var bookings = _context.Booking.Include(b => b.Customer).Where(b => b.CustomerId == customerId);
+
+            if (bookings == null)
+            {
+                return NotFound();
+            }
+
+            return bookings;
+        }
+
+        // POST: api/customer/postRatingReview?id=1
+        [HttpPost("PostRatingReview")]
+        [Authorize(Roles = "Customer")]
+        public async Task<IActionResult> PostRatingReview(int id, [FromBody] RatingReview ratingReview)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            int currentHotelId = id;
+            string customerId = User.Claims.First(c => c.Type == "UserID").Value;
+
+            var ratingReviewToBeCreated = new RatingReview()
+            {
+                CustomerId = customerId,
+                Rating = ratingReview.Rating,
+                Review = ratingReview.Review,
+                HotelId = currentHotelId
+            };
+
+            _context.RatingReview.Add(ratingReviewToBeCreated);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("PostRatingReview", new { id = ratingReview.Id }, ratingReview);
+        }
+
+        // PUT: api/customer/reviewed?id=1
+        [HttpPut("reviewed")]
+        [Authorize(Roles = "Customer")]
+        public async Task<IActionResult> ReviewedBooking(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            int currentBookingId = id;
+
+            string currentCustomerId = User.Claims.First(c => c.Type == "UserID").Value;
+
+            var booking = await _context.Booking.FindAsync(currentBookingId);
+
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            if (booking.CustomerId != currentCustomerId)
+            {
+                return Forbid();
+            }
+
+            booking.Reviewed = true;
+
+            _context.Entry(booking).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+
+            return Ok();
+        }
+
+        // PUT: api/customer/cancel?id=1
+        [HttpPut("cancel")]
+        [Authorize(Roles = "Customer")]
+        public async Task<IActionResult> CancelBooking(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            int currentBookingId = id;
+
+            string currentCustomerId = User.Claims.First(c => c.Type == "UserID").Value;
+
+            var booking = await _context.Booking.FindAsync(currentBookingId);
+
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            if (booking.CustomerId != currentCustomerId)
+            {
+                return Forbid();
+            }
+
+            booking.Canceled = true;
+
+            _context.Entry(booking).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+
+            return Ok();
+        }
+
+
+        private bool BookingExists(int id)
+        {
+            return _context.Booking.Any(e => e.Id == id);
+        }
+
     }
 }
